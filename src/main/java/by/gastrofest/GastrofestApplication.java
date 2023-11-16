805404019c8d0e9b78cdb286cf15cc53201dee36
Alexander Neznaev <corpsetrup@gmail.com>
2023-11-16 18:04:40 +0300

    initial commit

---
+++ src/main/java/by/gastrofest/GastrofestApplication.java
@@ -0,0 +1,13 @@
+package by.gastrofest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrofestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastrofestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/gastrofest/GastrofestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gastrofest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrofestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
